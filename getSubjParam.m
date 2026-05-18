a3e671417a0bc94b1f94a171ef90710e90e8fcdf
Merelvansteenis <merelvansteenis2003@gmail.com>
2026-05-18 16:17:10 +0200

    Seperating subj parameters in task types (auditory vs visual)

--- getSubjParam.m
+++ getSubjParam.m
@@ -3,9 +3,14 @@ function param = getSubjParam(pp)
 %% participant-specific notes
 
 %% set path and pp-specific file locations
-unique_numbers = [48, 39, 43, 32, 70, 30, 21, 79, 69, 94, 42, 53]; %needs to be in the right order
+unique_numbers = [68, 43, 75, 79, 29, 63, 77]; %needs to be in the right order
 
-param.path = '/Users/merelvansteenis/Documents/m6 - auditory/';
+%% set path to auditory task
+param.path.auditory = '/Users/merelvansteenis/Documents//Users/merelvansteenis/Documents/m6.2 - auditory vs visual/';
+
+%% Task-specific folders
+param.path_a = fullfile(param.path, 'Auditory/');
+param.path_v = fullfile(param.path, 'Visual/');
 
 if pp < 10
     param.subjName = sprintf('pp0%d', pp);
@@ -13,8 +18,36 @@ else
     param.subjName = sprintf('pp%d', pp);
 end
 
-log_string = sprintf('data_session_%d.csv', pp);
-param.log = [param.path, log_string];
+%% Determine the task type 
+% Odd pp: Auditory = session 1, Visual = session 2
+% Even pp: Auditory = session 2, Visual = session 1
 
-eds_string = sprintf('%d_%d.asc', pp, unique_numbers(pp));
-param.eds = [param.path, eds_string];
+if mod(pp,2) == 1
+    session_a = 1;
+    session_v = 2;
+else
+    session_a = 2;
+    session_v = 1;
+end
+
+%% Behavioural files
+% Auditory
+log_string_a = sprintf('data_session_%d_%d.csv', pp, session_a);
+param.log_a = fullfile(param.path_a, log_string_a);
+
+% Visual
+log_string_v = sprintf('data_session_%d_%d.csv', pp, session_v);
+param.log_v = fullfile(param.path_v, log_string_v);
+
+%% Eye data files
+% Auditory
+eds_string_a = sprintf('%d_%d_%d.asc', ...
+    pp, unique_numbers(pp), session_a);
+param.eds_a = fullfile(param.path_a, eds_string_a);
+
+% Visual
+eds_string_v = sprintf('%d_%d_%d.asc', ...
+    pp, unique_numbers(pp), session_v);
+param.eds_v = fullfile(param.path_v, eds_string_v);
+
+end
